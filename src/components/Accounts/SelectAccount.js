b178905d9a57bb84f0f3b83c4b4a64e750831a5c
Hari Govind <hgs@disroot.org>
2018-07-22 21:00:36 +0530

    Select Accont Lists Name and number

--- src/components/Accounts/SelectAccount.js
+++ src/components/Accounts/SelectAccount.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList,AsyncStorage, TouchableNativeFeedback,
+Button} from 'react-native';
 
 export default class SelectAccount extends React.Component {
+
+    async componentDidMount() {
+        record = await AsyncStorage.getItem('ACCOUNTS');
+        rec = JSON.parse(record);
+        this.setState({record: rec});
+        this.setState({recordLoaded: true });
+    }
+
     static navigationOptions = {
         title: 'Select Account',
       };
+     state ={
+         recordLoaded: false
+        }
+
+        renderSeparator = () => {
+            return (
+              <View
+                style={styles.seperator}
+              />
+            );
+        };
+
     render(){
         return(
             <View style={styles.container}>
@@ -12,6 +33,29 @@ export default class SelectAccount extends React.Component {
                     Select active account from below. 
                     Attendance details of the active user is shown in the attendance tab.
                 </Text>
+                <Button
+                    title="Clear"
+                    onPress={() => AsyncStorage.clear()}
+                />
+                {
+                 this.state.recordLoaded ? (    
+                <FlatList
+                    
+                    data={this.state.record}
+                    renderItem={({item}) => 
+                        <TouchableNativeFeedback 
+                        style={styles.optionContent}
+                        background={TouchableNativeFeedback.SelectableBackground()}>
+                            <View style={styles.option}>
+                            <Text style={styles.name}>{item.name}</Text>
+                            <Text style={styles.register}>{item.reg_no}</Text>
+                            </View>
+                        </TouchableNativeFeedback>  
+                        
+                    }
+                    ItemSeparatorComponent={this.renderSeparator}
+                />) : <Text>Loading...</Text>
+                }
             </View>
         );
     }
@@ -25,5 +69,26 @@ const styles = StyleSheet.create({
     info: {
         padding: 12,
         color: 'gray',
-    }
+    },
+    name: {
+        fontSize: 18,
+        justifyContent:'center',
+      },
+    register: {
+        color: 'gray',
+        justifyContent:'center',
+    },
+    separator: {
+        flexDirection:'row',
+        flex: 1,
+        height: 3,
+        backgroundColor: 'red',
+    },
+    option: {
+        padding:12,
+        justifyContent:'center',
+    },
+    optionContent: {
+
+    },
 });
