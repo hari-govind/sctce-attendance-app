e58e09048eec29a78e09f7f4ce5ff2068fa08ceb
Hari Govind <hgs@disroot.org>
2018-08-05 23:00:17 +0530

    Retriving detailed attendace JSON to detailed screen

--- src/components/Detailed.js
+++ src/components/Detailed.js
@@ -1,11 +1,68 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, ScrollView, Text, AsyncStorage} from 'react-native';
+import {getDetailsJSON} from './DataProcessor/dataProcessor.js';
 
 export default class Detailed extends React.Component {
+
+    async reloadData(){
+        this.setState({error: false})
+        try{
+            this.setState({isLoaded: false})
+            this.setState({ActiveAccount: (JSON.parse(await AsyncStorage.getItem('ActiveAccount')))})
+            ActiveAccount = this.state.ActiveAccount
+            this.setState({hasActiveRecord: true})
+            reactThis = this
+           getDetailsJSON(ActiveAccount.reg_no,ActiveAccount.password)
+            .then(function(data) {
+                return data;
+            })
+            .then(function(details){
+                reactThis.setState({detailed: details})
+                date = new Date()
+                updated_date = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
+                reactThis.setState({updated_date})
+                reactThis.setState({isLoaded: true})
+                reactThis.setState({loadedKey:reactThis.state.ActiveAccount.key})
+            })
+            .catch((err) => {
+                reactThis.setState({error: true})
+            })
+        } catch (err) {
+            this.setState({hasActiveRecord: false})
+            console.log('Err' + err)
+        }
+    }
+
+    async reloadIfNeeded() {
+        try{
+        active = JSON.parse(await AsyncStorage.getItem('ActiveAccount'))
+        if(active.key!=this.state.loadedKey){
+            this.reloadData()
+        }}
+        catch (err) {
+            this.setState({hasActiveRecord: false})
+        }
+    }
+
+    async componentDidMount(){
+        this.setState({error: false})
+        this.reloadData()
+    }
+
+state = {
+    isLoaded: false,
+}
     render(){
         return(
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Detailed Page!</Text>
+            {
+                this.state.isLoaded ? (
+                    <ScrollView>
+                    <Text>{JSON.stringify(this.state.detailed)}</Text>
+                    </ScrollView>
+                ) : <Text>Loading</Text>
+            }
+
         </View>
         );
     }
