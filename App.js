839e7c2f544668ef65df418f9f53bf54a76a62fa
Hari Govind <hgs@disroot.org>
2018-10-07 20:24:36 +0530

    added version number and splash screen logo larger

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
