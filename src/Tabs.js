cfeb9399419bcd19fc341b17c123e50d33a1dde2
Hari Govind <hgs@disroot.org>
2019-10-14 23:05:13 +0530

    Removed unwanted imports

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
-import { Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import Summary from './components/Summary';
